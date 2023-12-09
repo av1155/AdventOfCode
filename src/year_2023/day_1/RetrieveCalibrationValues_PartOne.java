98af716f9d7341d2e3f5d37f36f15d751104f285
Andrea Venti Fuentes <117413846+av1155@users.noreply.github.com>
2023-12-09 02:04:07 -0500

    First commit

---
+++ src/year_2023/day_1/RetrieveCalibrationValues_PartOne.java
@@ -0,0 +1,58 @@
+package year_2023.day_1;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class RetrieveCalibrationValues_PartOne
+{
+    public static void main(String[] args) throws FileNotFoundException
+    {
+        File inputFile = new File("src/year_2023/day_1/input.txt");
+        Scanner fileReader = new Scanner(inputFile);
+
+        int sum = 0, lineCounter = 0;
+
+        // Loop through each line in the text file
+        while (fileReader.hasNextLine())
+        {
+            // Store the line in a variable 'line' and increment the line counter
+            String line = fileReader.nextLine();
+            lineCounter++;
+
+            // Loop through each character in the line, and create two variables to store the first and last digit
+            String firstDigit = "", lastDigit = "";
+            for (int i = 0; i < line.length(); i++)
+            {
+                // Check if the character is a digit
+                if (Character.isDigit(line.charAt(i))) // Character.isDigit() checks if a character is a digit
+                {
+                    // If the first digit is empty, store the first digit in the firstDigit variable
+                    if (firstDigit.isEmpty())
+                    {
+                        firstDigit = String.valueOf(line.charAt(i)); // String.valueOf() converts char to String
+                    }
+
+                    // Store the last digit in the lastDigit variable
+                    lastDigit = String.valueOf(line.charAt(i));
+                }
+            }
+
+            // Check if the first and last digit are not empty
+            if (!firstDigit.isEmpty() && !lastDigit.isEmpty())
+            {
+                // Concatenate the first and last digit, and convert it to an integer
+                int concatenatedNumber = Integer.parseInt(firstDigit + lastDigit);
+
+                // Add the concatenated number to the sum and print the line number and the concatenated number
+                sum += concatenatedNumber;
+                System.out.printf("Line %d: %d\n", lineCounter, concatenatedNumber);
+            }
+        }
+
+        fileReader.close();
+
+        // Print the calibration value
+        System.out.printf("The calibration value is: %d\n", sum);
+    }
+}
---
+++ src/year_2023/day_1/RetrieveCalibrationValues_PartTwo.java
@@ -0,0 +1,100 @@
+package year_2023.day_1;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class RetrieveCalibrationValues_PartTwo
+{
+    public static void main(String[] args) throws FileNotFoundException
+    {
+        File inputFile = new File("src/year_2023/day_1/input.txt");
+        Scanner fileReader = new Scanner(inputFile);
+
+        // Two-dimensional array of Strings to identify the numbers in the text file, and their corresponding digits
+        String[][] letterNumbers = {
+            {"1", "one"  }, // letterNumbers[0][0] = "1", letterNumbers[0][1] = "one"
+            {"2", "two"  }, // letterNumbers[1][0] = "2", letterNumbers[1][1] = "two"
+            {"3", "three"}, // letterNumbers[2][0] = "3", letterNumbers[2][1] = "three"
+            {"4", "four" }, // letterNumbers[3][0] = "4", letterNumbers[3][1] = "four"
+            {"5", "five" }, // letterNumbers[4][0] = "5", letterNumbers[4][1] = "five"
+            {"6", "six"  }, // letterNumbers[5][0] = "6", letterNumbers[5][1] = "six"
+            {"7", "seven"}, // letterNumbers[6][0] = "7", letterNumbers[6][1] = "seven"
+            {"8", "eight"}, // letterNumbers[7][0] = "8", letterNumbers[7][1] = "eight"
+            {"9", "nine" }, // letterNumbers[8][0] = "9", letterNumbers[8][1] = "nine"
+        };
+
+        int sum = 0, lineCounter = 0;
+
+        while (fileReader.hasNextLine())
+        {
+            String line = fileReader.nextLine();
+            System.out.println(line);
+            lineCounter++;
+
+            String firstDigit = "", lastDigit = "";
+            String currentDigit = "";
+            boolean foundFirstDigit = false;
+
+            // Iterate over each character and check for digits and number words
+            for (int index = 0; index < line.length(); index++)
+            {
+                currentDigit = "";
+
+                // Check direct digit
+                if (Character.isDigit(line.charAt(index)))
+                {
+                    // If the current character is a digit, set the currentDigit to the digit
+                    currentDigit = String.valueOf(line.charAt(index));
+                }
+                else
+                {
+                    // Check for number words
+                    for (int j = 0; j < letterNumbers.length; j++)
+                    {
+                        // Iterate over each number word and check if the line starts with it at the current index
+                        String numericWord = letterNumbers[j][1];
+                        String correspondingDigit = letterNumbers[j][0];
+
+                        // If the line starts with the number word, set the currentDigit to the corresponding digit
+                        if (line.startsWith(numericWord, index))
+                        {
+                            // Set the current digit to the corresponding digit
+                            currentDigit = correspondingDigit;
+
+                            // Skip the length of the word
+                            index += numericWord.length() - 1;
+
+                            break;
+                        }
+                    }
+                }
+
+                // If the current digit is not empty, set the first digit to the current digit
+                if (!currentDigit.isEmpty())
+                {
+                    // If the first digit has not been found, set the first digit to the current digit
+                    if (!foundFirstDigit)
+                    {
+                        firstDigit = currentDigit;
+                        foundFirstDigit = true;
+                    }
+
+                    lastDigit = currentDigit;
+                }
+            }
+
+            // If the first and last digits are not empty, concatenate them and add them to the sum
+            if (!firstDigit.isEmpty() && !lastDigit.isEmpty())
+            {
+                int concatenatedNumber = Integer.parseInt(firstDigit + lastDigit);
+                sum += concatenatedNumber;
+                System.out.printf("Line %d: %d\n", lineCounter, concatenatedNumber);
+            }
+        }
+
+        fileReader.close();
+
+        System.out.printf("The calibration value is: %d\n", sum);
+    }
+}
